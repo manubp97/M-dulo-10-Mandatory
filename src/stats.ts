3d4f64556f42b9a2973cf15f161f75c8ac0c1a92
Manuel Bautista Paredes <manubp97@gmail.com>
2022-03-08 18:51:08 +0100

    Add files via upload
    First commit

---
+++ src/stats.ts
@@ -0,0 +1,168 @@
+export interface ResultEntry {
+  name: string;
+  value: number;
+}
+/*
+Casos acumulados por provincia (22 abril 2021)
+*/
+export const initialStats: ResultEntry[] = [
+  {
+    name: "Madrid",
+    value: 646763,
+  },
+  {
+    name: "La Rioja",
+    value: 28525,
+  },
+  {
+    name: "Andalucía",
+    value: 523865,
+  },
+  {
+    name: "Cataluña",
+    value: 553393,
+  },
+  {
+    name: "Valencia",
+    value: 387611,
+  },
+  {
+    name: "Murcia",
+    value: 109529,
+  },
+  {
+    name: "Extremadura",
+    value: 72442,
+  },
+  {
+    name: "Castilla La Mancha",
+    value: 179346,
+  },
+  {
+    name: "País Vasco",
+    value: 171232,
+  },
+  {
+    name: "Cantabria",
+    value: 26990,
+  },
+  {
+    name: "Asturias",
+    value: 48771,
+  },
+  {
+    name: "Galicia",
+    value: 118559,
+  },
+  {
+    name: "Aragón",
+    value: 113630,
+  },
+  {
+    name: "Castilla y León",
+    value: 217129,
+  },
+  {
+    name: "Islas Canarias",
+    value: 49630,
+  },
+  {
+    name: "Islas Baleares",
+    value: 58380,
+  },
+  {
+    name: "Navarra",
+    value: 56959,
+  },
+  {
+    name: "Ceuta",
+    value: 5515,
+  },
+  {
+    name: "Melilla",
+    value: 8279,
+  },
+];
+
+/*
+Casos acumulados por provincia (4 marzo 2022)
+*/
+
+export const finalStats: ResultEntry[] = [
+  {
+    name: "Madrid",
+    value: 1604494,
+  },
+  {
+    name: "La Rioja",
+    value: 89603,
+  },
+  {
+    name: "Andalucía",
+    value: 1350312,
+  },
+  {
+    name: "Cataluña",
+    value: 2286486,
+  },
+  {
+    name: "Valencia",
+    value: 1304243,
+  },
+  {
+    name: "Murcia",
+    value: 375894,
+  },
+  {
+    name: "Extremadura",
+    value: 236938,
+  },
+  {
+    name: "Castilla La Mancha",
+    value: 473631,
+  },
+  {
+    name: "País Vasco",
+    value: 653344,
+  },
+  {
+    name: "Cantabria",
+    value: 126294,
+  },
+  {
+    name: "Asturias",
+    value: 195213,
+  },
+  {
+    name: "Galicia",
+    value: 534214,
+  },
+  {
+    name: "Aragón",
+    value: 398524,
+  },
+  {
+    name: "Castilla y León",
+    value: 655412,
+  },
+  {
+    name: "Islas Canarias",
+    value: 297195,
+  },
+  {
+    name: "Islas Baleares",
+    value: 255498,
+  },
+  {
+    name: "Navarra",
+    value: 223910,
+  },
+  {
+    name: "Ceuta",
+    value: 18927,
+  },
+  {
+    name: "Melilla",
+    value: 20296,
+  },
+];
